3824b212a1d4bbfda07ac0bd8fb821e64a323a34
ambientstl <41842065+ambientstl@users.noreply.github.com>
2019-11-20 15:11:33 -0600

    first (failed) attempt at text game logic

---
+++ js development/some-logic.js
@@ -0,0 +1,29 @@
+// console.log("#################################");
+// console.log("### Welcome to the Cave Game! ###");
+// alert("#############################\n### Welcome to the Cave Game! ###\n#############################");
+
+// let name = prompt("Enter your name: ", "Human");
+// if (name.length > 1) {
+//     break
+// } else {
+//     // console.log("Please enter a name.")
+//     let name = prompt("Enter your name: ", "Human");
+// } 
+
+// alert("#############################\nYou find yourself outside THREE CAVES.\nThere is a STICK propped next to the entrance of the first CAVE.")
+
+function loop(stmt, ask) {
+    alert(stmt);
+    let response = prompt(ask);
+    if (response.length < 1) {
+        loop(stmt, ask);
+    }
+}
+
+const title = "#############################\n### Welcome to the Cave Game! ###\n#############################"
+let inputName = "Enter your name: "
+
+loop(title, inputName);
+
+// let stmt = "#############################\nYou find yourself outside THREE CAVES.\nThere is a STICK propped next to the entrance of the first CAVE.";
+// let ask = 
