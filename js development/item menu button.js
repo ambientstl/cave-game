1830a34dab05679cc3d8fc1fd86ce6f46a2575e7
ambientstl <41842065+ambientstl@users.noreply.github.com>
2019-11-22 13:32:03 -0600

    :sparkle: add item menu button functionality

--- js development/item menu button.js
+++ js development/item menu button.js
@@ -2,7 +2,8 @@ const player1 = {
   items: {
     potionCount: 2,
     bigPotionCount: 4
-  }
+  },
+  HP: 10
 };
 
 const thisCurrentMap = {
@@ -42,17 +43,89 @@ function drawItemMenu(player) {
     newButton.className = "btn btn-dark btn-lg potion-btn";
     // set button's inner html
     newButton.innerHTML = `Potion: ${potionCountArray[0]}`;
+    // add potion button event listener
+    addPotionEventListener("Potion");
   }
   // if big potion count > 0,
   if (potionCountArray[1]) {
     // create new button element
     const newButton = document.createElement("button");
     // add button classes to button element
-    newButton.className = "btn btn-dark btn-lg potion-btn";
+    newButton.className = "btn btn-dark btn-lg lg-potion-btn";
     // set button's inner html
     newButton.innerHTML = `Large Potion: ${potionCountArray[1]}`;
     // append button to main screen
     screen.appendChild(newButton);
+    // add potion button event listener
+    addPotionEventListener("Large Potion");
+  }
+}
+
+// using items from the item menu
+const potion = {
+  name: "potion",
+  heal: 5,
+  message: "Used Potion. Healed 5 HP."
+};
+
+const largePotion = {
+  name: "Large Potion",
+  heal: 15,
+  message: "Used Large Potion. Healed 15 HP."
+};
+
+// the potion button event listeners need to be added after the item menu is drawn
+// asynchronous functions? or just add function to drawItemMenu?
+
+// potion argument should be "Potion" or "Large Potion"
+function addPotionEventListener(potionType) {
+  // if potionType is potion
+  if (potionType === "Potion") {
+    document
+      .querySelector(".potion-btn")
+      .addEventListener("click", function usePotion() {
+        // decrease potion count in player object and item menu
+        player1.items.potionCount -= 1;
+        // apply potion's effects
+        player1.HP += potion.heal;
+        // TODO: update player HP in player info area
+        // TODO: check for maximum HP, change HP # in message if < 5
+        // display 'used potion' message in text area
+        document.querySelector(".text-area").innerHTML = potion.message;
+        // check potion count
+        // if potionCount is 0, remove button, and short-circuit exit function
+        if (!checkPotionCounts(player1)[0]) {
+          document.querySelector(".potion-btn").remove();
+          return;
+        }
+        // if potionCount > 0, update potion button
+        document.querySelector(
+          ".potion-btn"
+        ).innerHTML = `Potion: ${player1.items.potionCount}`;
+      });
+  } else if (potionType === "Large Potion") {
+    document
+      .querySelector(".lg-potion-btn")
+      .addEventListener("click", function usePotion() {
+        // decrease potion count in player object and item menu
+        player1.items.bigPotionCount -= 1;
+        // apply potion's effects
+        player1.HP += largePotion.heal;
+        // TODO: update player HP in player info area
+        // TODO: check for maximum HP, change HP # in message if < 15
+        // display 'used potion' message in text area
+        document.querySelector(".text-area").innerHTML = largePotion.message;
+        // check potion count
+        // if potionCount is 0, remove button, and short-circuit exit function
+        if (!checkPotionCounts(player1)[1]) {
+          document.querySelector(".lg-potion-btn").remove();
+          return;
+        }
+        // if potionCount > 0, update potion button
+        document.querySelector(
+          ".lg-potion-btn"
+        ).innerHTML = `Large Potion: ${player1.items.bigPotionCount}`;
+      });
   }
 }
 
