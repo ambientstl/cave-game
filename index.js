d3c7064b5a8b30995a86534826f557cb00c593d9
Manav Misra <manavm@visionwebsoft.com>
2019-12-07 12:44:14 -0600

    :construction: Write demo code and comments to intro possible SPA arch.

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+import { Player } from "./lib/players";
+import { HiddenMap } from "./lib/maps";
+
 import { Main } from "./components";
+
+console.log(new Player("Heelo Wrold"));
+
+const hm = new HiddenMap(
+  "hiddenMap1",
+  "Ghost Corridor",
+  "A haunted corridor.",
+  5,
+  "Ghost",
+  null,
+  // TODO: (future) make start and end = map objects instead of strings
+  "witchDen",
+  "caveOneFrontCavern"
+);
+
+console.log(hm);
 
 function render() {
   document.querySelector("#root").innerHTML = `
